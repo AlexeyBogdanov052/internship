0665e8655e11e5ee6f14d798b524d30b03283bfa
AlexeyBogdanov052 <aleksey.bogdanov.99@mail.ru>
2020-09-25 00:34:23 +0300

    First commit

---
+++ src/main/db/scripts/001.SAMPLE.sql
@@ -0,0 +1,5 @@
+create table staff (
+    emp_id int primary key,
+    emp_fullname varchar(255),
+    emp_hash varchar(255)
+);
---
+++ src/main/db/scripts/002.SAMPLE.sql
@@ -0,0 +1,1 @@
+insert into staff (emp_id, emp_fullname, emp_hash) values (1, 'Chris', 'adfhq235t5jkngadklsfioqen');
