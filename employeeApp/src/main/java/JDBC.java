bd45b5360b79d18f01707ce235de6b1829020eea
AlexeyBogdanov052 <aleksey.bogdanov.99@mail.ru>
2020-10-19 01:19:26 +0300

    Second part

--- employeeApp/src/main/java/JDBC.java
+++ employeeApp/src/main/java/JDBC.java
@@ -3,10 +3,9 @@ import java.sql.*;
 public class JDBC {
     public static void Task() {
         try {
-            Connection connection = null;
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:58925/staff", "postgres", "postres");
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:58925/staff", "postgres", "postres");
             Statement statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("INSERT INTO scheme_rights (operation_name) values(...)");
+            ResultSet result = statement.executeQuery("INSERT INTO scheme_rights (operation_name) values(nextval(content_id_seq), '...')");
 
             result.close();
             statement.close();
